cd9b7439204994994c0da04c601d62526eca2c91
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-19 19:36:23 +0300

    new commit

--- src/actions/game/stopRound.ts
+++ src/actions/game/stopRound.ts
@@ -2,18 +2,18 @@ import { Issue } from '../../types/issue';
 import { Store } from '../../types/room';
 import { getVotingStatistics } from './getStatistics';
 
-export const stopRound = (
-  roomId: string,
-  store: Store
-): { currentRound: null; issue: Issue } => {
-  const room = store[roomId];
-  const issueWithStatistics = getVotingStatistics(
-    roomId,
-    room.currentRound?.issueId as string,
-    store
-  );
+// export const stopRound = (
+//   roomId: string,
+//   store: Store
+// ): { currentRound: null; issue: Issue } => {
+//   const room = store[roomId];
+//   const issueWithStatistics = getVotingStatistics(
+//     roomId,
+//     room.currentRound?.issueId as string,
+//     store
+//   );
 
-  room.currentRound = null;
+//   room.currentRound = null;
 
-  return { currentRound: null, issue: issueWithStatistics };
-};
+//   return { currentRound: null, issue: issueWithStatistics };
+// };
